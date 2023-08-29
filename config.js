93a9d78f9ea962afd0818108b9de93ab85a2ce8e
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-08-30 01:03:51 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-BOT_NUMBER: process.env.BOT_NUMBER === undefined ? '' : process.env.BOT_NUMBER,
-};
